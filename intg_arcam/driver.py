c5f4035c5881f91b653ba213d91d85d90572eb9e
Meir Miyara <3089864+mase1981@users.noreply.github.com>
2026-02-12 19:07:25 -0600

    Revert to v1.1.1 pattern to restore working setup flow

--- intg_arcam/driver.py
+++ intg_arcam/driver.py
@@ -46,22 +46,3 @@ class ArcamDriver(BaseIntegrationDriver[ArcamDevice, ArcamConfig]):
             ArcamSoundModeSensor(device_config, device),
             ArcamSoundModeSelect(device_config, device),
         ]
-
-    def register_available_entities(
-        self, device_config: ArcamConfig, device: ArcamDevice
-    ) -> None:
-        """Register entities as both available and configured."""
-        device_id = self.get_device_id(device_config)
-        _LOG.info("Registering entities for %s", device_id)
-
-        entities = self.create_entities(device_config, device)
-
-        for entity in entities:
-            if self.api.available_entities.contains(entity.id):
-                self.api.available_entities.remove(entity.id)
-            self.api.available_entities.add(entity)
-
-            if self.api.configured_entities.contains(entity.id):
-                self.api.configured_entities.remove(entity.id)
-            self.api.configured_entities.add(entity)
-            _LOG.info("Registered entity: %s", entity.id)
